36df9358eeb17697e5bc9bb79cc6e87a4c1116a5
erfan <erfan.esma1382@gmail.com>
2024-07-22 02:39:07 +0330

    Revert "fix Login"
    This reverts commit 00ca18762b3546db52f8372f760966a1fed03060.

--- app.js
+++ app.js
@@ -3,19 +3,7 @@ let last = "adf";
 const num1 = "1";
 console.log(name);
 function name(params) {
-  console.log(name);
-  return false;
+  num
 }
-const commit = "test branch";
-let branch = "master barnch";
-
-let arr = [0, 1, 2, 3];
-let isLogin = true;
-let num = "erfan";
-let lastName = "esmaeili";
-function user(num) {
-  return false;
-}
-const isSignIn = () => {
-  return false;
-};
+const commit = 'test branch'
+let branch = 'master barnch'
