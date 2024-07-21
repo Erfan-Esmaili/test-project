83ea93506630d20a0e4c40dd571c17c5cd1da1b5
erfan <erfan.esma1382@gmail.com>
2024-07-22 02:37:26 +0330

    isSign In

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-let name = 'ali'
-let last ='adf'
-const num = '1'
+let name = "ali";
+let last = "adf";
+const num1 = "1";
 console.log(name);
 function name(params) {
   console.log(name);
   return false;
 }
-const commit = 'test branch'
-let branch = 'master barnch'
+const commit = "test branch";
+let branch = "master barnch";
+
+let arr = [0, 1, 2, 3];
+let isLogin = true;
+let num = "erfan";
+let lastName = "esmaeili";
+function user(num) {
+  return false;
+}
+const isSignIn = () => {
+  return false;
+};
--- doc.js
+++ doc.js
@@ -1,5 +1,7 @@
 let num = [1,2,3]
-let btn = 'select'
-function fun (res){
-  console.log('asd');
-}
+let last = 'esma'
+num.forEach(element => {
+  console.log(element);
+});
+console.log(num);
+ 
