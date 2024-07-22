e12773b343ef9c3982f441811e3ae6b45844983d
erfan <erfan.esma1382@gmail.com>
2024-07-23 01:09:36 +0330

    move to github

--- app.js
+++
@@ -1,9 +0,0 @@
-let name = "ali";
-let last = "adf";
-const num1 = "1";
-console.log(name);
-function name(params) {
-  num
-}
-const commit = 'test branch'
-let branch = 'master barnch'
--- doc.js
+++
@@ -1,5 +0,0 @@
-let num = [1,2,3]
-let btn = 'select'
-function fun (res){
-  console.log('asd');
-}
